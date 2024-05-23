484559d25fdf79f6a01f394c43fd03ba7a2ed0d1
Niphemi00 <134238348+Niphemi00@users.noreply.github.com>
2024-05-23 06:06:26 +0100

    Update 16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;

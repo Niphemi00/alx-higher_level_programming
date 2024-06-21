0021f9e32fca05a2ba7bd83301917337a86ffd12
Niphemi00 <134238348+Niphemi00@users.noreply.github.com>
2024-06-21 15:00:05 +0100

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
-const args = process.argv.slice(2);
-
-if (args.length < 2) {
-    console.log("Please provide two arguments.");
-} else {
-    const [firstArg, secondArg] = args;
-    console.log(`${firstArg} is ${secondArg}`);
-}
+#!/usr/bin/node
+console.log(process.argv[2] + ' is ' + process.argv[3]);

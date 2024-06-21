57267e500bcdae4beab4d8a02559d3cc75d3b466
Niphemi00 <134238348+Niphemi00@users.noreply.github.com>
2024-06-21 14:59:27 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-console.log(args[0] ? args[0] : "No argument");
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}

de43aaa1676b8441f50554b19c7286c279842452
Niphemi00 <134238348+Niphemi00@users.noreply.github.com>
2024-07-04 23:21:33 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to get the size of the body of the response from a URL
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
+# Fetch and display the size of the response body
+curl -s "$1" | wc -c

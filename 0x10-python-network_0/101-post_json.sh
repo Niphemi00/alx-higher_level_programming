9e38eaac8754009896ccee8bca09ae52196d4449
Niphemi00 <134238348+Niphemi00@users.noreply.github.com>
2024-07-04 23:40:53 +0100

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a given URL with a given JSON file.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+# Send a POST request with JSON data from a file and display the response body
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
